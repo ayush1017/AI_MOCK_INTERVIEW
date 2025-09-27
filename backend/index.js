f592d344281d0ac5b8c5f9bed6866d8b728dc45d
ayush1017 <ayushsingh16900@gmail.com>
2025-09-27 20:04:59 +0530

    Completed API Integration and Changed Origin to true

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-  origin: "https://ai-mock-interview-4qpg.vercel.app/",//true.
+  origin: true,//true.
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
